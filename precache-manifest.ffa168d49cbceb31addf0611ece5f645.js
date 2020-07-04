d489daef2132474d743f1da32b9bd024ddced75d
Alberto Riolly <albert.r.cahyantara@gmail.com>
2020-07-04 14:21:59 +0700

    Updates

---
+++ precache-manifest.ffa168d49cbceb31addf0611ece5f645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44652a97d1fdabc44820588d30e3dcf",
+    "url": "/monsters-cat/index.html"
+  },
+  {
+    "revision": "f9cd0f10c9942978d910",
+    "url": "/monsters-cat/static/css/main.e3ad392b.chunk.css"
+  },
+  {
+    "revision": "30ff922f61e75086b083",
+    "url": "/monsters-cat/static/js/2.ff504122.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-cat/static/js/2.ff504122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9cd0f10c9942978d910",
+    "url": "/monsters-cat/static/js/main.44d95506.chunk.js"
+  },
+  {
+    "revision": "f79fbc23f3a4211b342e",
+    "url": "/monsters-cat/static/js/runtime-main.7ca60031.js"
+  }
+]);
